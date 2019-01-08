93c2dbd62be8045a2922f29b02ff1cc569bcb01f
ignacio.lasaosa <ignacio.lasaosa@futureconnections.com>
2019-01-08 12:49:17 +0100

    function setMessage.

--- MessageStore/MessageStore.sol
+++ MessageStore/MessageStore.sol
@@ -3,9 +3,13 @@ pragma solidity ^0.4.24;
 contract MessageStore {
 
 	address private owner;
+	string private message;
 
 	constructor() public {
 		owner = msg.sender;
 	}
 
+	function setMessage(string newMessage) public {
+		message = newMessage;
+	}
 }
