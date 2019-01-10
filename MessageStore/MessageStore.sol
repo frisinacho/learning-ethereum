ef66fa759e1fda172485af3a34b6fa188e97ab1b
ignacio.lasaosa <ignacio.lasaosa@futureconnections.com>
2019-01-10 12:40:45 +0100

    return Balance in ether.

--- MessageStore/MessageStore.sol
+++ MessageStore/MessageStore.sol
@@ -18,4 +18,8 @@ contract MessageStore is Ownable {
 	function getBalance() public view returns (uint) {
 		return address(this).balance;
 	}
+
+	function getBalanceInEther() public view returns (uint) {
+		return getBalance() / 1e18;
+	}
 }
